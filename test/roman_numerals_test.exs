670cce504ccf52f0e88dc60f5754c86f409bd709
Jeremy Schuurmans <jschuurmans@8thlight.com>
2020-03-31 11:36:27 -0500

    refactor

--- test/roman_numerals_test.exs
+++ test/roman_numerals_test.exs
@@ -2,11 +2,10 @@ defmodule RomanNumeralsTest do
   use ExUnit.Case
   doctest RomanNumerals
 
-  test "it returns 'M' for 1000" do
+  test "when passed in a value that directly maps to a numeral, it returns the numeral" do
     assert RomanNumerals.convert(1000) == "M"
-  end
-
-  test "it returns 'CM' for 900" do
-    assert RomanNumerals.convert(900) == "CM"
+    assert RomanNumerals.convert(900)  == "CM"
+    assert RomanNumerals.convert(500)  == "D"
+    assert RomanNumerals.convert(400)  == "CD"
   end
 end
