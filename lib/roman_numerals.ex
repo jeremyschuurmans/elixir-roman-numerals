d99a354b8f1f7cfb1341d2a23a97d209c07556ee
Jeremy Schuurmans <jschuurmans@8thlight.com>
2020-03-31 08:53:22 -0500

    first commit

---
+++ lib/roman_numerals.ex
@@ -0,0 +1,18 @@
+defmodule RomanNumerals do
+  @moduledoc """
+  Documentation for `RomanNumerals`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> RomanNumerals.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/roman_numerals_test.exs
@@ -0,0 +1,8 @@
+defmodule RomanNumeralsTest do
+  use ExUnit.Case
+  doctest RomanNumerals
+
+  test "greets the world" do
+    assert RomanNumerals.hello() == :world
+  end
+end
