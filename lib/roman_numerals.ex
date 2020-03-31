224e423375ff50da7a1627d90b4cedc82024738a
Jeremy Schuurmans <jschuurmans@8thlight.com>
2020-03-31 14:42:09 -0500

    refactor test and solution for 1000

--- lib/roman_numerals.ex
+++ lib/roman_numerals.ex
@@ -1,25 +1,33 @@
 defmodule RomanNumerals do
-  def convert(number) do
-     base_values = %{
-        1000 => "M",
-         900 => "CM",
-         500 => "D",
-         400 => "CD",
-         100 => "C",
-          90 => "XC",
-          50 => "L",
-          40 => "XL",
-          10 => "X",
-           9 => "IX",
-           5 => "V",
-           4 => "IV",
-           1 => "I"
-      }
 
-    cond do
-      base_values[number] ->
-        base_values[number]
-    end
+  def convert(number) do
+       # 1000 => "M",
+       #  900 => "CM",
+       #  500 => "D",
+       #  400 => "CD",
+       #  100 => "C",
+       #   90 => "XC",
+       #   50 => "L",
+       #   40 => "XL",
+       #   10 => "X",
+       #    9 => "IX",
+       #    5 => "V",
+       #    4 => "IV",
+       #    1 => "I"
+    
+    thousands = div(number, 1000)
+    hundreds  = div(rem(number, 1000), 100)
+    tens      = div(rem(number, 100), 10)
+    ones      = rem(number, 10)
+
+    numeral = String.duplicate("M", thousands)
+
+    
+     
+
+
+
+    
   end
 end
 
--- test/roman_numerals_test.exs
+++ test/roman_numerals_test.exs
@@ -2,10 +2,7 @@ defmodule RomanNumeralsTest do
   use ExUnit.Case
   doctest RomanNumerals
 
-  test "when passed in a value that directly maps to a numeral, it returns the numeral" do
+  test "it converts 1000 to 'M'" do
     assert RomanNumerals.convert(1000) == "M"
-    assert RomanNumerals.convert(900)  == "CM"
-    assert RomanNumerals.convert(500)  == "D"
-    assert RomanNumerals.convert(400)  == "CD"
   end
 end
